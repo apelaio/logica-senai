bd0fe64d279ef9c38408b93e05ad94275c5da658
jlog180924 <jlog180924@info.local>
2018-09-25 22:11:16 -0300

    Criação da classe Soma

---
+++ aulas/workspace/operadores-java/src/aritimeticos/Soma.java
@@ -0,0 +1,10 @@
+package aritimeticos;
+
+public class Soma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("É hora de dar tchau");
+	}
+
+}
